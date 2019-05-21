06ae787a6de12780bd147727a56bb88326be4b5c
soapia <sofia.ongele@ealas.org>
2019-05-21 10:36:32 -0400

    iteration 1 and done

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let alertController = UIAlertController()
+    @IBAction func loveEmoji(_ sender: Any) {
+        alertController.addAction(UIAlertAction(title: "i love you", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+
+
+    }
+    @IBAction func nerdEmoji(_ sender: Any) {
+        alertController.addAction(UIAlertAction(title: "mitochondria is the powerhouse of the cell", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func emojYee(_ sender: Any) {
+        alertController.addAction(UIAlertAction(title: "i got the horses in the back", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func ohNoji(_ sender: Any) {
+        alertController.addAction(UIAlertAction(title: "don't worry be happy!", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
