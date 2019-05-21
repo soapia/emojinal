7a42adba40c61cb049ec062848af87b1a600e0e3
soapia <sofia.ongele@ealas.org>
2019-05-21 10:45:40 -0400

    iteration 1 and done but this time i followed the instructions

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -11,23 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     let alertController = UIAlertController()
-    @IBAction func loveEmoji(_ sender: Any) {
-        alertController.addAction(UIAlertAction(title: "i love you", style: UIAlertAction.Style.default, handler: nil))
+    @IBAction func showMessage(sender: UIButton) {
+        
+        alertController.addAction(UIAlertAction(title: "hey whats up hello", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
 
 
-    }
-    @IBAction func nerdEmoji(_ sender: Any) {
-        alertController.addAction(UIAlertAction(title: "mitochondria is the powerhouse of the cell", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
-    }
-    @IBAction func emojYee(_ sender: Any) {
-        alertController.addAction(UIAlertAction(title: "i got the horses in the back", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
-    }
-    @IBAction func ohNoji(_ sender: Any) {
-        alertController.addAction(UIAlertAction(title: "don't worry be happy!", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
+        
     }
     
     override func viewDidLoad() {
