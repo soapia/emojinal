3402b37d41610ab14d34a97c3f957d1827511cc4
soapia <sofia.ongele@ealas.org>
2019-05-21 11:14:14 -0400

    iteration 2 with constraints that are trying their best

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let alertController = UIAlertController()
+    // PROBLEM AREA its not stated that you have to begin filling out the inside of UIAlertController and INSIDE the function so your alert wont be the same type unless you do...
+    
+    let emojis = ["🥰": "lovey dovey", "🤓": "nerdy", "🤠": "yee yee", "😰": "stressed"]
     @IBAction func showMessage(sender: UIButton) {
-        
-        alertController.addAction(UIAlertAction(title: "hey whats up hello", style: UIAlertAction.Style.default, handler: nil))
+        // PROBLEM AREA kinda fixed with a lil google search but swift gets MAD with the ? on the optional bc the types wont match, needs to be unwraped with !
+        let selectedEmotion = sender.titleLabel!.text
+       let alertController = UIAlertController(title: "how we feelin?", message: emojis[selectedEmotion!], preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "okay", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
 
 
